909081519123f9386686d328e649e3453ca5d5c2
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-04-25 21:00:50 -0400

    Added ability to submit code to the engine

---
+++ js/submit.js
@@ -0,0 +1,48 @@
+var form = document.getElementById("code-submit-form");
+var apiUrl =  "http://localhost:8000/submit";
+
+function readCodeFile(event) {
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        console.log("Done reading file.");
+        rawResult = reader.result; // base64 encoded
+        data = rawResult.substring(26) // discard substring "data:text/plain;base64,"
+        payload = '{"code": "' + data + '"}'
+        $.ajax({
+            type: "POST",
+            url: apiUrl,
+            contentType: "text/plain",
+            success: handleAjaxSuccess,
+            error: handleAjaxError,
+            data: payload
+        });
+    }, false);
+
+    if (file) {
+        console.log("Reading file...")
+        reader.readAsDataURL(file);
+    }
+};
+
+$('#code-submit-form').submit(function () {
+    readCodeFile();
+    return false;
+});
+
+function handleAjaxSuccess(response) {
+    console.log("AJAX success!");
+    console.log(response);
+    if (response.success) {
+        document.getElementById('result-text').innerHTML = "Success!";
+    } else {
+        document.getElementById('result-text').innerHTML = "Failed.";
+    }
+}
+
+function handleAjaxError(response) {
+    console.log("AJAX error!");
+    console.log(response);
+    document.getElementById('result-text').innerHTML = "AJAX error!";
+}
