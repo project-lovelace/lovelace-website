e1943a8b8513cde65f1d1c93c7f2d27ae5350197
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-05-30 18:01:59 -0400

    Created directory for project-wide templates

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lovelace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
