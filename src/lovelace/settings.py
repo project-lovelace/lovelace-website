b358a3013be31c240bd1ba1129ffc7a0205b29e5
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-05-31 16:18:40 -0400

    Deleted Django secret key
    We'll create a new one which we'll actually keep secret and automate pasting it in during deployment.

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u5s4$^ta#=#j9+mno!87@+h!$8u6hx^pi#mfi-f31)i&c6)w^b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
