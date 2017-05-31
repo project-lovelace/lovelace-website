7e2584e439381e0186a2b4f8dc29cb61b9669407
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-05-31 15:16:58 -0400

    Deleted old lines from the Django tutorial
    They were needed for the "polls" app the tutorial get you to build.

--- src/problems/urls.py
+++ src/problems/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /problems/5/
     url(r'^(?P<problem_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
