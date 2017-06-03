409ad8ee554f277041c2778a074e066eb720542f
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-06-03 10:26:27 -0400

    Configured ALLOWED_HOSTS to allow Django to run on our server

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'foobar'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.projectlovelace.net']
 
 
 # Application definition
