7afaa80509a04ebd61669bc248cd11b052a1dc6b
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-12-31 15:00:19 -0500

    Removed unnecessary "load static", fixed tutorial page rendering

--- src/lovelace/urls.py
+++ src/lovelace/urls.py
@@ -1,18 +1,3 @@
-"""lovelace URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -30,14 +15,14 @@ urlpatterns = [
     url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 
     # User accounts
-    url(r'^register/$', views.UserRegistrationView.as_view(), name='register'),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^register$', views.UserRegistrationView.as_view(), name='register'),
+    url(r'^login$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout$', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Temporary pages
+    url(r'^tutorials/$', TemplateView.as_view(template_name='temporary/tutorials.html'), name='tutorials'),
+    url(r'^tutorials/1$', TemplateView.as_view(template_name='temporary/tutorial_1.html')),
 
     # The problems app
     url(r'^problems/', include('problems.urls'), name='problems'),
-
-    # Temporary
-    url(r'^tutorials$', TemplateView.as_view(template_name='temporary/tutorials.html'), name='tutorials'),
-    url(r'^tutorials/1$', TemplateView.as_view(template_name='temporary/tutorial_1.html')),
 ]
