5578b3022bd5602a49befc145231c0a74f38d027
Basim Ramadhan <basim.ramadhan@gmail.com>
2018-01-23 10:01:17 -0500

    Small fixes to get the Engine to run in production

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Consult the deployment checklist before deploying to production:
-# https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+# https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'foobar'
