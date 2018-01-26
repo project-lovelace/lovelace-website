c2c0f927eb05cfab4960da4087f77630e52bc650
Basim Ramadhan <basim.ramadhan@gmail.com>
2018-01-26 18:46:34 -0500

    Improved rendering and validation of login and registrations forms

--- src/lovelace/views.py
+++ src/lovelace/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
@@ -13,23 +13,33 @@ class UserRegistrationView(View):
     form_class = UserRegistrationForm
     template_name = 'registration/register.html'
 
-    # Display blank registration form
+    def _partially_filled_form(self, request, form, model_errors=None):
+        """Display registration form with valid fields pre-filled."""
+        return render(request, self.template_name, {
+                'form': form,
+                'model_errors': model_errors,
+            })
+
     def get(self, request):
+        """Display blank registration form."""
         form = self.form_class(data=None)
         return render(request, self.template_name, {'form': form})
 
-    # Process form data and register the user
     def post(self, request):
+        """Process form data and register the user."""
         form = self.form_class(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']  # TODO unnecessary?
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = User.objects.create_user(username, email, password)  # TODO disallow bad passwords
-            profile = UserProfile(user=user, display_name=username)
-            profile.save()
-            # user.groups.add('users')  # TODO create a group for normal users?
-            login(request, user)
-            return redirect(to='home')
-        else:
-            return render(request, self.template_name, {'form': form})
+        if not form.is_valid():
+            return self._partially_filled_form(request, form)
+
+        user = form.save(commit=False)
+        profile = UserProfile(user=user, display_name=user.username)
+        try:
+            user.full_clean()
+            profile.full_clean()
+        except ValidationError as e:
+            return self._partially_filled_form(request, form, model_errors=e.message_dict)
+
+        user.save()
+        profile.save()
+        login(request, user)
+        return redirect(to='home')
