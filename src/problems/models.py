27889e8c665fba23d36558dace644934702b872a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-19 20:32:52 -0400

    Added solved by, difficulty, and timesink fields.

--- src/problems/models.py
+++ src/problems/models.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from users.models import UserProfile
 
@@ -10,6 +11,11 @@ class Problem(models.Model):
     name = models.CharField(unique=True, max_length=256)  # ex: "earthquake-epicenters"
     title = models.CharField(max_length=256)  # shown to user, ex: "Finding Earthquake Epicenters"
     date_added = models.DateField(default=date.today, editable=True)
+    solved_by = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    difficulty = models.IntegerField(default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(15)])
+    timesink = models.IntegerField(default=1,
+        validators=[MinValueValidator(1), MinValueValidator(15)])
     visible = models.BooleanField(default=False)
 
     def __str__(self):
