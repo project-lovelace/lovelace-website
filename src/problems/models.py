7d618ea5108ed3abdc003672ddaee9a2125ae3e5
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-19 20:53:08 -0400

    Migrated to new problem model.

--- src/problems/models.py
+++ src/problems/models.py
@@ -8,14 +8,23 @@ from users.models import UserProfile
 
 class Problem(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(unique=True, max_length=256)  # ex: "earthquake-epicenters"
-    title = models.CharField(max_length=256)  # shown to user, ex: "Finding Earthquake Epicenters"
+
+    # Unique name for database and urls, e.g. "earthquake-epicenters".
+    name = models.CharField(unique=True, max_length=256, editable=True)
+
+    # Human-readable name shown to user, e.g. "Finding Earthquake Epicenters".
+    title = models.CharField(max_length=256, editable=True)
+
     date_added = models.DateField(default=date.today, editable=True)
-    solved_by = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
+    solved_by = models.IntegerField(default=0, validators=[MinValueValidator(0)], editable=True)
+
     difficulty = models.IntegerField(default=1,
-        validators=[MinValueValidator(1), MaxValueValidator(15)])
+        validators=[MinValueValidator(1), MaxValueValidator(15)], editable=True)
+
     timesink = models.IntegerField(default=1,
-        validators=[MinValueValidator(1), MinValueValidator(15)])
+        validators=[MinValueValidator(1), MaxValueValidator(15)], editable=True)
+
     visible = models.BooleanField(default=False)
 
     def __str__(self):
