9c5af9f6d309d02b83e9343b94dbc643a269e8a3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-21 10:01:09 -0400

    User profiles work.

--- src/users/views.py
+++ src/users/views.py
@@ -1,17 +1,20 @@
+import logging
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views import View
 
 from .models import UserProfile
 
-# logger = logging.getLogger(__name__)  # TODO enable logging
+logger = logging.getLogger('django.' + __name__)
 
 
 class UserProfileView(View):
     template_name = 'users/profile.html'
 
     def get(self, request, username):
-        user = User.objects.get(username=username)
+        logger.info("Getting user profile for user {:}.".format(request.user))
+        user = User.objects.get(username=request.user)
         profile = UserProfile.objects.get(user=user)
         return render(request, self.template_name, {
             'user_shown': user,
