c48fe09216f34ea3747292cc5741db38667cc207
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-25 10:10:22 -0400

    Expanded UserProfile model. Haven't migrated yet.

--- src/users/models.py
+++ src/users/models.py
@@ -1,11 +1,20 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
+# from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
-    display_name = models.CharField(max_length=256)
+    display_name = models.CharField(max_length=40)
+    about = models.CharField(max_length=1000, default="")
+    birthday = models.DateField()
+    # country = CountryField()
+    location = models.CharField(max_length=50, default="")
+    avatar = models.ImageField(upload_to='avatars', max_length=100)
+    languages_used = ArrayField(models.CharField(max_length=20))
 
     def __str__(self):
         return '{} ({})'.format(self.display_name, self.user.username)
