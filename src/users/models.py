185919468956c4e29e73cad77540e46ad07b35c9
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-26 11:14:57 -0400

    User about and location can be blank.

--- src/users/models.py
+++ src/users/models.py
@@ -12,10 +12,10 @@ class UserProfile(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     display_name = models.CharField(max_length=40, default="")
-    about = models.CharField(max_length=1000, default="")
+    about = models.CharField(max_length=1000, blank=True)
     birthday = models.DateField(blank=True, null=True)
     country = CountryField(blank=True, null=True)
-    location = models.CharField(max_length=50, default="")
+    location = models.CharField(max_length=50, blank=True)
     avatar = models.ImageField(upload_to='avatars', max_length=100, default=static("img/default_avatar.png"))
 
     def __str__(self):
