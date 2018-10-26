8ecac603c5381eddb255a1f4b9006b52ab0b2d60
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-26 12:20:50 -0400

    Empty UserProfile created on user registration.

--- src/lovelace/views.py
+++ src/lovelace/views.py
@@ -4,6 +4,7 @@ import pprint
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 from .forms import CustomUserCreationForm
 from users.models import UserProfile
@@ -20,3 +21,16 @@ class UserRegistrationView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+    def post(self, request, *args, **kwargs):
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            UserProfile.objects.create(user=user)
+            return redirect('login')
+        else:
+            form = CustomUserCreationForm()
+
+        return render(request, self.template_name, {'form': form})
