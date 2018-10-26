e809604032f301d6bc55acf54dad06e3ee90b131
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-25 23:09:09 -0400

    Added country field to user profile model.

--- src/users/models.py
+++ src/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.templatetags.static import static
 
-# from django_countries.fields import CountryField
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
@@ -14,7 +14,7 @@ class UserProfile(models.Model):
     display_name = models.CharField(max_length=40)
     about = models.CharField(max_length=1000, default="")
     birthday = models.DateField(default=datetime.date(1900, 1, 1))
-    # country = CountryField()
+    country = CountryField(default='CA')
     location = models.CharField(max_length=50, default="")
     avatar = models.ImageField(upload_to='avatars', max_length=100, default=static("img/default_avatar.png"))
 
