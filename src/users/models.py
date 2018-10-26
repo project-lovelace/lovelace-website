873f1c37dc6f4b1e5d4963540aaba9ba0833f138
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-25 21:02:10 -0400

    Fixed UserProfile default avatar filepath.

--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+from django.templatetags.static import static
 
 # from django_countries.fields import CountryField
 
@@ -10,11 +13,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     display_name = models.CharField(max_length=40)
     about = models.CharField(max_length=1000, default="")
-    birthday = models.DateField()
+    birthday = models.DateField(default=datetime.date(1900, 1, 1))
     # country = CountryField()
     location = models.CharField(max_length=50, default="")
-    avatar = models.ImageField(upload_to='avatars', max_length=100)
-    languages_used = ArrayField(models.CharField(max_length=20))
+    avatar = models.ImageField(upload_to='avatars', max_length=100, default=static("img/default_avatar.png"))
 
     def __str__(self):
         return '{} ({})'.format(self.display_name, self.user.username)
