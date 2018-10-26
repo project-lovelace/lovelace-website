084c084fd2925b047a5f4d4f5afa924297d5c16a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-26 16:21:08 -0400

    Help text and validators for user model.

--- src/users/models.py
+++ src/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.templatetags.static import static
 from django.conf import settings
+from django.core.validators import MinValueValidator, MinLengthValidator, MaxLengthValidator
 
 from django_countries.fields import CountryField
 
@@ -17,14 +18,25 @@ def avatar_file_name(instance, filename):
 class UserProfile(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
-    display_name = models.CharField(max_length=40, default="")
-    about = models.CharField(max_length=1000, blank=True)
+
+    display_name = models.CharField(max_length=40, default="",
+            validators=[MinLengthValidator(3), MaxLengthValidator(40)],
+            help_text="Minimum and maximum length of 3-40 characters.")
+
+    about = models.CharField(max_length=1000, blank=True,
+            validators=[MaxLengthValidator(1000)],
+            help_text="Maximum length of 1000 characters.")
+
     birthday = models.DateField(blank=True, null=True)
     country = CountryField(blank=True, null=True)
-    location = models.CharField(max_length=50, blank=True)
+
+    location = models.CharField(max_length=50, blank=True,
+            validators=[MaxLengthValidator(50)],
+            help_text="Maximum length of 50 characters.")
+
     avatar = models.ImageField(upload_to=avatar_file_name, max_length=100, default=default_avatar_filepath)
-    problems_solved = models.IntegerField(default=0)
-    submissions_made = models.IntegerField(default=0)
+    problems_solved = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    submissions_made = models.IntegerField(default=0, validators=[MinValueValidator(0)])
 
     def __str__(self):
         return '{} ({})'.format(self.display_name, self.user.username)
