679f0dd9b62b09252735f08f8b73470507a5550b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-03 14:57:19 -0400

    Usernames must be 3-20 characters.

--- src/lovelace/forms.py
+++ src/lovelace/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 from django_registration import validators
 from django_registration.forms import RegistrationForm
@@ -13,6 +14,9 @@ class CustomRegistrationForm(RegistrationForm):
     """
     def __init__(self, *args, **kwargs):
         super(CustomRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields[User.USERNAME_FIELD].validators.append(MinLengthValidator(3))
+        self.fields[User.USERNAME_FIELD].validators.append(MaxLengthValidator(20))
+
         self.fields[User.USERNAME_FIELD].validators.append(
             validators.CaseInsensitiveUnique(
                 User, User.USERNAME_FIELD,
@@ -28,7 +32,8 @@ class CustomRegistrationForm(RegistrationForm):
             )
         )
 
-        self.fields['password1'].help_text = "Your password must contain at least 8 characters and can't be too similar to your other personal information, a commonly used password, or entirely numeric."
+        self.fields['username'].help_text = "Required. 3-20 characters. Letters, digits and @/./+/-/_ only."
+        self.fields['password1'].help_text = "Your password must contain at least 8 characters, cannot be entirely numeric, and cannot be too similar to your other personal information or a commonly used password."
 
 
 class CustomUserCreationForm(UserCreationForm):
