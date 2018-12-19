603672a9aba29760b26dc0b0e6bdb6d92388ad7c
Baz <basim.ramadhan@gmail.com>
2018-12-18 19:20:22 -0500

    Added development version of settings.py

---
+++ src/lovelace/settings.py
@@ -0,0 +1,201 @@
+import os
+
+# Consult the deployment checklist before deploying to production:
+# https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Consult the deployment checklist before deploying to production:
+# https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-secret-key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'localhost',
+]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'problems.apps.ProblemsConfig',
+    'users.apps.UsersConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'django_countries',
+    'widget_tweaks',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     }
+# }
+
+ROOT_URLCONF = 'lovelace.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # project-level templates directory
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'lovelace.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projectlovelace',
+        'USER': 'admin',
+        'PASSWORD': 'database-password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Toronto'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Directory for media files
+# Mostly used for storing user submitted files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# Look for static files in the project-level static files directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# When running collectstatic in production, put all static files into src/prod_static
+STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static')
+
+# When client requests files under projectlovelace.net/static, look for them in STATIC_ROOT
+STATIC_URL = '/static/'
+
+# Login URL used especially by @login_required decorator.
+LOGIN_URL = '/accounts/login/'
+
+# Redirect users after logging in and logging out
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+# SSL/HTTPS settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# HTTP Strict Transport Security
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True  # KEEP THIS OFF UNLESS YOU UNDERSTAND THE CONSEQUENCES!
+
+# More recommended security settings
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Persist database connections for better performance
+# KEEP OFF UNTIL WE KNOW THIS IS LOWER THAN POSTGRES' "IDLE CONNECTION TIMEOUT"
+# CONN_MAX_AGE = 600
+
+# Email server settings
+# See: https://docs.djangoproject.com/en/2.1/topics/email/
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'postmaster@mg.projectlovelace.net'
+EMAIL_HOST_PASSWORD = 'mailgun-password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "Ada Lovelace <ada@mg.projectlovelace.net>"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '[%(asctime)s] %(name)s.%(levelname)s (%(pathname)s:%(lineno)d): %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/lovelace/lovelace-django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+# django-registration settings
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window
+
+# Discourse settings.
+# See: https://meta.discourse.org/t/sso-example-for-django/14258
+DISCOURSE_BASE_URL = 'http://discourse.projectlovelace.net'
+DISCOURSE_SSO_SECRET = 'discourse-sso-secret'
