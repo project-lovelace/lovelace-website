f84096cedad22f9580fd7ba6115082f855453dde
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-04-14 22:49:41 -0400

    Django docker works

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -76,17 +76,16 @@ WSGI_APPLICATION = 'lovelace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projectlovelace',
-        'USER': 'admin',
-        'PASSWORD': 'lovelace',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
+#  DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'projectlovelace',
+#          'USER': 'admin',
+#          'PASSWORD': 'lovelace',
+#          'HOST': 'localhost',
+#          'PORT': '5432',
+#      }
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
