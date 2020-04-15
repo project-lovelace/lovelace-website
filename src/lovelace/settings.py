333033d8c997458efe0a8c75ba73c7a853c34f14
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-04-15 09:28:05 -0400

    Compose a postgres docker

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'lovelace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#  DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': 'projectlovelace',
-#          'USER': 'admin',
-#          'PASSWORD': 'lovelace',
-#          'HOST': 'localhost',
-#          'PORT': '5432',
-#      }
-#  }
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
