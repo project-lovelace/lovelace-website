3ea4298467785071130a78610a8de47c83d82782
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-04-14 22:49:24 -0400

    Start using docker-compose and hiding sensitive info

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -10,18 +10,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-secret-key'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
-
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
-
 INSTALLED_APPS = [
     'problems.apps.ProblemsConfig',
     'users.apps.UsersConfig',
@@ -180,7 +178,7 @@ LOGGING = {
             'level': 'DEBUG',
             'formatter': 'simple',
             'class': 'logging.FileHandler',
-            'filename': '/var/log/lovelace/lovelace-django.log',
+            'filename': 'lovelace-django.log',
         },
     },
     'loggers': {
