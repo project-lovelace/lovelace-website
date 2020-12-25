cccf515fb0783b6bcc835a1b2fe25f1aa80f9f68
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-12-25 11:14:52 -0500

    Serve media files through nginx

--- src/lovelace/settings.py
+++ src/lovelace/settings.py
@@ -102,9 +102,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-# Directory for media files
-# Mostly used for storing user submitted files
+# Directory for media files. Mostly used for storing user submitted files.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
