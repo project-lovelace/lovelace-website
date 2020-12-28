713945ae158640b139e7b19d07cdb695cd6295d8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-12-28 17:41:26 -0500

    Success with plotting chaos output with Plotly!

---
+++ src/static/visualization/chaos.js
@@ -0,0 +1,40 @@
+function format_input(input, output, expected, n) {
+    document.getElementById(`input${n}`).innerHTML = `<b>Input</b>: r = ${input[0]} <br>`
+    return
+}
+
+function format_output(input, output, expected, n) {
+    var r = input[0]
+    var x_expected = expected[0]
+    var x_output = output[0]
+
+    var iters_expected = [...x_expected.keys()]
+    var iters_output = [...x_output.keys()]
+
+    var trace_expected = {
+      x: iters_expected,
+      y: x_expected,
+      mode: 'lines+markers',
+      name: 'expected'
+    }
+
+    var trace_output = {
+      x: iters_output,
+      y: x_output,
+      mode: 'lines+markers',
+      name: 'your output'
+    }
+
+    var layout = {
+        title:`Logistic map with r=${r}`,
+        xaxis: { title: 'iterations' },
+        yaxis: { title: 'x' },
+        paper_bgcolor: 'rgba(0, 0, 0, 0)',
+        plot_bgcolor: 'rgba(0, 0, 0, 0)'
+    }
+
+    document.getElementById(`output${n}`).innerHTML = `<b>Output</b>: <br> <div id="output${n}plot"></div>`
+    Plotly.newPlot(`output${n}plot`, [trace_expected, trace_output], layout)
+
+    return
+}
